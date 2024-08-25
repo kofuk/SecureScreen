131a90684a60402bfda5edaa6e9efe2bc9af0736
Koki Fukuda <ko.fu.dev@gmail.com>
2024-08-25 17:29:37 +0900

    Add Quick Settings tile

---
+++ app/src/main/java/com/chronoscoper/android/securescreen/PinningTileService.kt
@@ -0,0 +1,20 @@
+package com.chronoscoper.android.securescreen
+
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.N)
+class PinningTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+
+        startActivity(Intent(this, SecureActivity::class.java).apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+        })
+    }
+}
