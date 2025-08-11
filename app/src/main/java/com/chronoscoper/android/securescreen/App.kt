a71283179a5cd0dcfbf192c868536f837a23c5aa
Koki Fukuda <ko.fu.dev@gmail.com>
2025-08-11 10:54:29 +0900

    Update copyright year

--- app/src/main/java/com/chronoscoper/android/securescreen/App.kt
+++ app/src/main/java/com/chronoscoper/android/securescreen/App.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 Koki Fukuda
+ * Copyright 2019-2025 Koki Fukuda
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
